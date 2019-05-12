bf4c114ae1171f2668cd6b9df1f6cc27e4b1cbb3
kentatogashi <togashik@togashiknoMacBook-puro.local>
2019-05-12 18:03:47 +0900

    Add data_structure directory

---
+++ data_structure/binary_tree.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.data)
+        if self.right:
+            self.right.PrintTree()
+
+root = Node(100)
+root.insert(50)
+root.insert(150)
+root.PrintTree()
